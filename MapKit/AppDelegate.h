1f69ee2c2826d98d9c100ceb57c8404afdc5119f
zhuming3834 <zhuming3834@sina.com>
2015-10-21 20:00:48 +0800

    Initial Commit

---
+++ MapKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapKit
+//
+//  Created by HGDQ on 15/10/21.
+//  Copyright (c) 2015年 HGDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapKit
+//
+//  Created by HGDQ on 15/10/21.
+//  Copyright (c) 2015年 HGDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
